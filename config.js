b006d3028bf40f825cbc5df1c682c8330c11d230
leon mak <leonmakansheng@gmail.com>
2016-10-02 20:41:52 +0800

    log token

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAId9aYT1GMBADZCREaOUvTIcvLt83c9rlbk4yzYBgHZA4yjtMpZAR1Cp6G0MoCM1rZA8Yo9Fl9jZCpmaoRwvHpjjRSDYhy9ZCj4wNwQ86cBAZC0EfDZC3LZAMFKqGCZAsKCrDF5l9VavPmtbFtVhULIkSSwUtcFMv6wnjF4RlhUjRcwZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'my_voice_is_my_password_verify_me'
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
